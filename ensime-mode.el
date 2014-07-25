80238a294c741b4ce804d7fba74b3b40a7070cb9
Andreas Kellers <andreas.kellers@gmail.com>
2014-07-25 14:31:58 +0200

    added filename conversion for cache dir in ensime--create-server-start-script

--- ensime-mode.el
+++ ensime-mode.el
@@ -524,7 +524,7 @@ CACHE-DIR is the server's persistent output directory."
 (defun ensime--create-server-start-script (scala-version cache-dir config-file active)
   ;; emacs has some weird case-preservation rules in regexp replace
   ;; see http://github.com/magnars/s.el/issues/62
-  (s-replace-all (list (cons "_cache_dir_" (expand-file-name cache-dir))
+  (s-replace-all (list (cons "_cache_dir_" (ensime-cygwin-filename-to-win (expand-file-name cache-dir)))
 		       (cons "_scala_version_" scala-version)
 		       (cons "_active_module_" active)
 		       (cons "_config_file_" (expand-file-name config-file)))
