85ec75d0a6155100353dea7749267f33efa4ec6f
Andreas Kellers <andreas.kellers@gmail.com>
2014-08-28 17:53:21 +0200

    minor reorganization

--- ensime-cygwin-utils.el
+++ ensime-cygwin-utils.el
@@ -47,11 +47,24 @@ backslashes with forward slashes and removing then Cygwin root."
   (ensime-cygwin-filename-to-cyg (mapconcat 'identity
 				     (split-string f "\\\\") "/")))
 
-(defun ensime-cygwin-convert-to-unix (s)
-  "Convert string by replacing double backslashes to single
-forward slashes and removing cygwin root"
-  (replace-regexp-in-string ensime-cygwin-root ""
-			    (replace-regexp-in-string "\\\\\\\\" "/" s)))
+
+(defun ensime-cygwin-convert-backslashes (s)
+  "Convert double backslashes to single forward slashes."
+  (replace-regexp-in-string "\\\\\\\\" "/" s))
+
+(defun ensime-cygwin-convert-backslash (s)
+  "Convert double backslashes to single forward slashes."
+  (replace-regexp-in-string "\\\\" "/" s))
+
+(defun ensime-cygwin-convert-cygwin (s)
+  "Convert cygwin root value with empty string"
+  (replace-regexp-in-string ensime-cygwin-root "" s))
+
+(defun ensime-cygwin-convert (s)
+  "Convert string using backslash and cygwin conversion"
+  (ensime-cygwin-convert-cygwin 
+   (ensime-cygwin-convert-backslash 
+    (ensime-cygwin-convert-backslashes s))))
 
 (defun ensime-cygwin-change-slashes (s)
   "Replace backward slashes with forward ones"
